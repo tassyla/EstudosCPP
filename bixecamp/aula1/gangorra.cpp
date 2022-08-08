fdd8cd33632de4c74ece4c8bbb0edfeaa4494bc2
Tássyla Lima <tassyla.lima@dtidigital.com.br>
2022-08-08 15:19:41 -0300

    Primeiros códigos

---
+++ bixecamp/aula1/gangorra.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int p1, c1, p2, c2;
+
+    cin >> p1 >> c1 >> p2 >> c2;
+    
+    if(p1*c1 > p2*c2)
+        cout << -1;
+    else if (p1*c1 < p2*c2)
+        cout << 1;
+    else
+        cout << 0;
+
+    return 0;
+}
---
+++ bixecamp/aula1/todos-divisores.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int x;
+
+    cin >> x;
+
+    for(int i=1; i<=x; i++)
+        if(x % i == 0)
+            cout << i << " ";
+    
+    cout << "\n";
+    
+    return 0;
+}
